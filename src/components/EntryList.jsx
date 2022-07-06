95102123ce085577997d47e3c5930b83daad0e71
Sonny949 <sonnypeterson949@hotmail.com>
2022-07-06 13:46:43 +1000

    deploying

---
+++ src/components/EntryList.jsx
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import StoreContext from "../store";
+import EntryListItem from "./EntryListItem";
+
+function EntryList() {
+  const { 
+		store: { entries } 
+	} = useContext(StoreContext);
+
+  return entries ? (
+    <ul>
+      {entries.map((entry) => (
+        <EntryListItem id={entry._id} text={entry.entry} />
+      ))}
+    </ul>
+  ) : (
+    <p>Loading ...</p>
+  )
+}
+
+export default EntryList;
